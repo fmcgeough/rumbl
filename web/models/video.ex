32c986a27612fd3bda24926be3848e5dd0e39737
fmcgeough <fmcgeough@gmail.com>
2016-07-30 11:00:07 -0400

    initial commit of project from Programming Phoenix

---
+++ web/models/video.ex
@@ -0,0 +1,26 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+
+    timestamps()
+  end
+
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:url, :title, :description])
+    |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
+  end
+end
