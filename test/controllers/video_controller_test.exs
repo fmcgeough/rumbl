73721d4f33f88f34b4f9cfd268a71bb852d8ae1f
fmcgeough <fmcgeough@gmail.com>
2016-07-30 11:11:38 -0400

    add ExUnit tagging for fake user login

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,27 +1,32 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
-  setup do
-    user = insert_user(username: "max")
-    conn = assign(build_conn(), :current_user, user)
-    {:ok, conn: conn, user: user}
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: "max")
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
   end
 
-  # test "requires user authentication on all actions", %{conn: conn} do
-  #   Enum.each([
-  #     get(conn, video_path(conn, :new)),
-  #     get(conn, video_path(conn, :index)),
-  #     get(conn, video_path(conn, :show, "123")),
-  #     get(conn, video_path(conn, :edit, "123")),
-  #     put(conn, video_path(conn, :update, "123", %{})),
-  #     post(conn, video_path(conn, :create, %{})),
-  #     delete(conn, video_path(conn, :delete, "123")),
-  #     ], fn conn ->
-  #       assert html_response(conn, 302)
-  #       assert conn.halted
-  #     end)
-  # end
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each([
+      get(conn, video_path(conn, :new)),
+      get(conn, video_path(conn, :index)),
+      get(conn, video_path(conn, :show, "123")),
+      get(conn, video_path(conn, :edit, "123")),
+      put(conn, video_path(conn, :update, "123", %{})),
+      post(conn, video_path(conn, :create, %{})),
+      delete(conn, video_path(conn, :delete, "123")),
+      ], fn conn ->
+        assert html_response(conn, 302)
+        assert conn.halted
+      end)
+  end
 
+  @tag login_as: "max"
   test "lists all user's videos on index", %{conn: conn, user: user} do
     user_video = insert_video(user, title: "funny cats")
     other_video = insert_video(insert_user(username: "other"), title: "another video")
